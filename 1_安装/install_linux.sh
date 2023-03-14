0e98ce4404195550ef3486ee45280fa3be54a94f
SongJi <719792323@qq.com>
2023-03-14 17:09:17 +0800

    2023_3_14

---
+++ 1_安装/install_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 以下载安装2.3.2版本为例
+# 设置IP代理（可选）
+set http_proxy=192.168.0.233:7890
+set https_proxy=192.168.0.233:7890
+
+# 同步时间
+timedatectl set-timezone Asia/Shanghai
+
+# 下载
+wget https://github.com/prometheus/prometheus/releases/download/v2.3.2/prometheus-2.3.2.linux-amd64.tar.gz
+
+# 解压
+tar -zxvf prometheus-2.3.2.linux-amd64.tar.gz
+
+# 移动文件到/usr/local
+cp prometheus-2.3.2.linux-amd64/prometheus /usr/local/bin/
+cp prometheus-2.3.2.linux-amd64/promtool /usr/local/bin/
+
+# 输出版本信息
+prometheus --version
---
+++ 5_webui/原生UI/webui使用说明.md
@@ -0,0 +1,5 @@
+# 访问WebUi
+1. 确保正确启动prometheus服务
+2. 访问prometheus服务节点的9090端口
+
+# WebUi功能介绍
